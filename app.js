e86ea197526b300a6b422ca3aaf465b5b79dc407
Juan Pablo <jballes89830@universidadean.edu.co>
2021-08-03 12:58:31 -0500

    programacion de ventana modal

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const audienciaImg = document.getElementById("audiencia");
 const capacidades = document.querySelector(".capacidades");
 const navBarr = document.querySelector(".navBarr");
 const divPreguntas = document.getElementById("preguntas");
+const closeBtn = document.querySelector(".closeBtn");
+const fondoModal = document.querySelector(".fondoModal");
+const tituloModal = document.querySelector(".tituloModal");
+const textoModal = document.querySelector(".textoModal");
+const imagenModal = document.querySelector(".imagenModal");
+const modal = document.querySelector(".modal");
 
 window.addEventListener("scroll", () => {
     imgCabecera.style.backgroundPositionY = `-${window.scrollY / 3}px`;
@@ -31,6 +37,44 @@ const preguntas = ["Qué canales sociales usan",
     "Qué oportunidades encuentra en lo observado", "Cómo puede mejorar la situación actual de la empresa en social media y la situación actual de la audiencia"
 ];
 
+const textos = [
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error rem perspiciatis ex numquam
+    quisquam qui itaque eaque corporis sint iste voluptatum accusantium, ipsam explicabo aliquid cupiditate
+    incidunt dolor doloremque?`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error rem perspiciatis ex numquam
+    quisquam qui itaque eaque corporis sint iste voluptatum accusantium, ipsam explicabo aliquid cupiditate
+    incidunt dolor doloremque?`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error rem perspiciatis ex numquam
+    quisquam qui itaque eaque corporis sint iste voluptatum accusantium, ipsam explicabo aliquid cupiditate
+    incidunt dolor doloremque?`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error rem perspiciatis ex numquam
+    quisquam qui itaque eaque corporis sint iste voluptatum accusantium, ipsam explicabo aliquid cupiditate
+    incidunt dolor doloremque?`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error rem perspiciatis ex numquam
+    quisquam qui itaque eaque corporis sint iste voluptatum accusantium, ipsam explicabo aliquid cupiditate
+    incidunt dolor doloremque?`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error rem perspiciatis ex numquam
+    quisquam qui itaque eaque corporis sint iste voluptatum accusantium, ipsam explicabo aliquid cupiditate
+    incidunt dolor doloremque?`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error rem perspiciatis ex numquam
+    quisquam qui itaque eaque corporis sint iste voluptatum accusantium, ipsam explicabo aliquid cupiditate
+    incidunt dolor doloremque?`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error rem perspiciatis ex numquam
+    quisquam qui itaque eaque corporis sint iste voluptatum accusantium, ipsam explicabo aliquid cupiditate
+    incidunt dolor doloremque?`
+];
+
+const imagenes = [
+    "https://images.unsplash.com/photo-1516251193007-45ef944ab0c6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1432888622747-4eb9a8efeb07?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80",
+    "https://images.unsplash.com/photo-1588466596988-f1e6f59370b9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=696&q=80",
+    "https://images.unsplash.com/photo-1576267423445-b2e0074d68a4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1434626881859-194d67b2b86f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=753&q=80",
+    "https://images.unsplash.com/photo-1462899006636-339e08d1844e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+];
+
 const armarDivs = i => {
     const span = document.createElement("SPAN");
     const div = document.createElement("DIV");
@@ -41,7 +85,7 @@ const armarDivs = i => {
     divPreguntas.appendChild(div);
 
     div.addEventListener("click", () => {
-        console.log(i);
+        abrirModal(preguntas[i], textos[i], imagenes[i]);
     });
 }
 
@@ -52,3 +96,24 @@ const mapear = () => {
 }
 
 mapear();
+
+closeBtn.addEventListener("click", () => {
+    fondoModal.style.display = "none";
+    document.body.style.overflowY = "scroll";
+});
+
+const abrirModal = (titulo, texto, imagen) => {
+    modal.style.maxWidth = "0px";
+    fondoModal.style.display = "flex";
+    document.body.style.overflow = "hidden";
+    tituloModal.textContent = titulo;
+    textoModal.textContent = texto;
+    imagenModal.style.backgroundImage = `url(${imagen})`;
+    let ancho = 0;
+    setInterval(() => {
+        if(ancho < 850){
+            ancho += 10;
+            modal.style.maxWidth = `${ancho}px`;
+        }
+    }, 1);
+}
